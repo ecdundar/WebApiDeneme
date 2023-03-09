a00c8e53ab200fe13458abc401bfde75064183fd
Eyüp Can Dündar <eyup@ecdyazilim.com>
2023-03-09 10:21:34 +0300

    EmployeeController eklendi

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BurulasWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/employee")]
+    public class EmployeeController : ControllerBase
+    {
+        public String Ping()
+        {
+            return "Hello World";
+        }
+    }
+}
