edd8a2c499109d10d421161ed7ba755e204b9b0f
Eyüp Can Dündar <eyup@ecdyazilim.com>
2023-03-09 10:18:58 +0300

    GetQuery ve GetList eklendi

---
+++ Helpers/DBHelper.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.SqlClient;
+using Dapper;
+
+namespace BurulasWebApi.Helpers
+{
+    public static class DBHelper
+    {
+        private static String ConnectionString = "";
+        public static T GetQuery<T>(string _sql, object _prms = null)
+        {
+            using (var con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                try
+                {
+                    return con.Query<T>(_sql, _prms).FirstOrDefault();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+        public static List<T> GetList<T>(string _sql, object _prms = null)
+        {
+            using (var con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                try
+                {
+                    return con.Query<T>(_sql, _prms).ToList();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+    }
+}
