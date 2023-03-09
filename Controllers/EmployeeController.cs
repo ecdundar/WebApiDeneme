a1a1ab192208f008932edfc6d40e5b9efbb9f082
Eyüp Can Dündar <eyup@ecdyazilim.com>
2023-03-09 10:58:54 +0300

    Get Employee

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -8,10 +8,21 @@ namespace BurulasWebApi.Controllers
     [Route("api/employee")]
     public class EmployeeController : ControllerBase
     {
-        [HttpGet("GetList")]
+        //[HttpGet("GetList")]
         public async Task<ActionResult<ListResponse<Models.Employee>>> GetList()
         {
             var employees = await Task.FromResult(EmployeeService.Instance.GetList());
+            if (employees == null)
+            {
+                return NotFound();
+            }
+            return employees;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<StandardResponse<Models.Employee>>> Get(int id)
+        {
+            var employees = await Task.FromResult(EmployeeService.Instance.Get(id));
             if (employees == null)
             {
                 return NotFound();
--- Services/EmployeeService.cs
+++ Services/EmployeeService.cs
@@ -12,5 +12,13 @@ namespace BurulasWebApi.Services
             result.Status = true;
             return result;
         }
+
+        public StandardResponse<Employee> Get(int id)
+        {
+            var result = new StandardResponse<Employee>();
+            result.Data = DBHelper.GetQuery<Employee>("select * from Employee with (nolock) where EmployeeID=@ID", new { ID = id });
+            result.Status = true;
+            return result;
+        }
     }
 }
